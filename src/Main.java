d1db8e2ae4d223ebd941cabe41e1496f5008b589
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2020-10-08 12:18:47 +0200

    implemented basic programm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * @author Paul Duschek
+ * @version 1.0, 8.10.20
+ */
+
 public class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Geben Sie 2 zu addierende Zahlen ein:%n");
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        System.out.printf("Ergebnis: %d", val1 + val2);
+    }
+
 }
